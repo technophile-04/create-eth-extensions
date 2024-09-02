258239106b466391160ff50cc8a731791e0bdc0f
Carlos Sánchez <oceanrdn@gmail.com>
2024-09-02 17:04:12 +0200

    add solidityVersion + networks

--- extension/packages/hardhat/hardhat.config.ts.args.mjs
+++ extension/packages/hardhat/hardhat.config.ts.args.mjs
@@ -1,1 +1,12 @@
 export const imports = `import "./tasks"`;
+export const solidityVersion = "0.8.16";
+export const networks = `
+    myFakeNetwork: {
+      url: \`https://my-fake-networt.alchemyapi.io/v2/\${providerApiKey}\`,
+      accounts: [deployerPrivateKey],
+    },
+    myFakeNetwork2: {
+      url: \`https://my-fake-networt2.alchemyapi.io/v2/\${providerApiKey}\`,
+      accounts: [deployerPrivateKey],
+    }
+`;
